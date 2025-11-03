e3793c3055f0d75f2be08761849a48c1d7d31a7d
Edison <edisonchepkwony59@gmail.com>
2025-11-03 13:44:37 +0300

    after life

--- frontend/login.js
+++ frontend/login.js
@@ -4,14 +4,26 @@ const validateForm = () => {
     if(Email.value !== "" && passWord.value !== "")
     {
         console.log("user logged in successfully");
+        window.location.href = "/frontend/home.html";
+        // alert("You have loggged in successfully");
+        
+        
+    }else{
+        console.log("skill issue");
         Email.value = "";
         passWord.value = "";
-        window.location.href = "form.html";
-    }else{
-        alert("skill issue");
     }
+    
 }
+// Email.addEventListener("input", () => {
+//     //clear old custom messages so native validation can work
+//     Email.setCustomValidity("");
 
+//     //If still invalid after reset, add custom message
+//     if (!Email.validity.valid){
+//         Email.setCustomValidity("Acha eeh wei");
+//     }
+// })
 
 
 
